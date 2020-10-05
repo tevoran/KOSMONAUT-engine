f33e078e46523a0b44d40a110aed01c6d6b82801
tevoran <j.pfeiffer93@gmx.de>
2020-10-05 15:45:20 +0200

    removed potential copyright protected box texture

--- src/main.c
+++ src/main.c
@@ -59,15 +59,6 @@ int main()
 	r=1.4*PI;
 	gfx_model_rotate(ship, r, rot_axis);
 
-	struct model *model[10000];
-
-	for(int i=0; i<10000; i++)
-	{
-		model[i]=gfx_copy_model(ship);
-		pos_model.z=pos_model.z+25;
-		gfx_update_model_location(model[i],pos_model);
-	}
-
 	int FPS=0;
 
 	struct vec3f color;
@@ -78,27 +69,18 @@ int main()
 	pos_model.x=10;
 	pos_model.y=10;
 	pos_model.z=30;
-	struct model *cube=gfx_create_cube(pos_model, color, 4);
+	struct model *cube_origin=gfx_create_cube(pos_model, color, 4);
+	gfx_model_load_texture("data/textures/box.bmp", cube_origin);
 
-	pos_model.x=-10;
-	color.x=0.9;
-	color.y=0.9;
-	color.z=0.21;
+	struct model *cube[1000];
 
-	struct vec3f corner1, corner2;
-	corner1.x=4;
-	corner1.y=0;
-	corner1.z=0;
-	corner2.x=0;
-	corner2.y=4;
-	corner2.z=0;
-	struct model *tri=gfx_create_triangle(pos_model, corner1, corner2, color);
+	for(int i=0; i<1000; i++)
+	{
+		cube[i]=gfx_copy_model(cube_origin);
+		pos_model.z=pos_model.z+100;
+		gfx_update_model_location(cube[i],pos_model);
+	}
 
-	pos_model.x=20;
-
-	color.x=0.34;
-	gfx_model_load_texture("data/textures/box.bmp", cube);
-	gfx_model_load_texture("data/textures/smiley.bmp", tri);
 	int lasttick=SDL_GetTicks();
 
 	float i=0;
@@ -118,8 +100,13 @@ int main()
 	rot_axis.y=1;
 	rot_axis.z=0;
 	r=0.001;
-	gfx_model_rotate(ship, r, rot_axis);
-	gfx_model_rotate(cube, r, rot_axis);
+	gfx_model_rotate(cube_origin, r, rot_axis);
+
+	for(int i=0; i<1000; i++)
+	{
+		gfx_model_rotate(cube[i], r, rot_axis);
+	}
+
 
 	gfx_new_frame();
 	}
