f089bac3977de38d3e525a304840d5750423a09f
tevoran <j.pfeiffer93@gmx.de>
2020-10-05 17:26:04 +0200

    a short test of moving the ship and the camera with the keyboard

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "general/general.h"
 #include "gfx/gfx.h"
@@ -18,22 +19,22 @@ int main()
 		break;
 		
 		case ENGINE_SDL_ERROR:
-		printf("ERROR: SDL ERROR\n");
+		engine_log("ERROR: SDL ERROR\n");
 		return 1;
 		break;
 		
 		case ENGINE_GL_ERROR:
-		printf("ERROR: OpenGL ERROR\n");
+		engine_log("ERROR: OpenGL ERROR\n");
 		return 1;
 		break;
 		
 		case ENGINE_GLEW_ERROR:
-		printf("ERROR: GLEW ERROR\n");
+		engine_log("ERROR: GLEW ERROR\n");
 		return 1;
 		break;
 		
 		case ENGINE_GFX_ERROR:
-		printf("ERROR: graphics engine error\n");
+		engine_log("ERROR: graphics engine error\n");
 		return 1;
 		break;
 		
@@ -43,15 +44,20 @@ int main()
 	gfx_create_camera(cam_location, 10000, 0.5*PI);
 
 
+	/*ship*/
 	struct vec3f rot_axis;
 	rot_axis.x=0;
 	rot_axis.y=1;
 	rot_axis.z=0;
-	struct vec3f pos_model={0,0,40};
-	struct model* ship=gfx_load_model("data/models/stealth/stealth.obj",pos_model);
+	struct vec3f ship_pos={0,0,40};
+	struct model* ship=gfx_load_model("data/models/stealth/stealth.obj",ship_pos);
 	gfx_model_load_texture("data/textures/stealth.bmp", ship);
 	float r=0.6*PI;
 	gfx_model_rotate(ship, r, rot_axis);
+	struct vec3f ship_current_rotation;
+	ship_current_rotation.x=0;
+	ship_current_rotation.y=0;
+	ship_current_rotation.z=0;
 
 	rot_axis.x=0;
 	rot_axis.y=1;
@@ -59,16 +65,9 @@ int main()
 	r=1.4*PI;
 	gfx_model_rotate(ship, r, rot_axis);
 
-	int FPS=0;
 
-	struct vec3f color;
-	color.x=0.8;
-	color.y=0.2;
-	color.z=0.21;
-
-	pos_model.x=10;
-	pos_model.y=10;
-	pos_model.z=30;
+	struct vec3f color={0,0,0};
+	struct vec3f pos_model={10,10,30};	
 	struct model *cube_origin=gfx_create_cube(pos_model, color, 4);
 	gfx_model_load_texture("data/textures/box.bmp", cube_origin);
 
@@ -77,14 +76,24 @@ int main()
 	for(int i=0; i<1000; i++)
 	{
 		cube[i]=gfx_copy_model(cube_origin);
-		pos_model.z=pos_model.z+100;
+		pos_model.z=pos_model.z+75;
 		gfx_update_model_location(cube[i],pos_model);
 	}
 
-	int lasttick=SDL_GetTicks();
 
-	float i=0;
-	while(engine_get_event().type!=SDL_QUIT) /*while not closing the window the main loop is continuing*/
+	/*time related stuff*/
+	int FPS=0;
+	int lasttick=SDL_GetTicks();
+	clock_t old_time=clock();
+	clock_t new_time, frame_time;
+	float frame_time_f;
+
+	float cube_rotation_speed=1; /*1 radian per second*/
+	float ship_speed=50;
+	float ship_rotation_speed=2.3;
+
+	int quit=0;
+	while(!quit) /*while not closing the window the main loop is continuing*/
 	{
 	/*FPS count*/
 	if(SDL_GetTicks()>(lasttick+1000))
@@ -96,12 +105,67 @@ int main()
 	}
 	FPS++;
 
+	/*determining time*/
+	new_time=clock();
+	frame_time=new_time-old_time;
+	old_time=new_time;
+	frame_time_f=(float)frame_time/CLOCKS_PER_SEC;
+	printf("F: %i us\n", (int)(frame_time_f*1000000));
+
+	/*handling input via SDL2*/
+	{
+		const Uint8 *keyboard_state=SDL_GetKeyboardState(NULL); 
+		SDL_Event event;
+
+		if(keyboard_state[SDL_SCANCODE_W])
+		{
+			ship_pos.z=ship_pos.z+ship_speed*frame_time_f*cos(ship_current_rotation.y);
+			ship_pos.x=ship_pos.x+ship_speed*frame_time_f*sin(ship_current_rotation.y);
+		}
+
+		if(keyboard_state[SDL_SCANCODE_S])
+		{
+			ship_pos.z=ship_pos.z-ship_speed*frame_time_f*cos(ship_current_rotation.y);
+			ship_pos.x=ship_pos.x-ship_speed*frame_time_f*sin(ship_current_rotation.y);
+		}
+
+		if(keyboard_state[SDL_SCANCODE_A])
+		{
+			ship_current_rotation.y=ship_current_rotation.y-ship_rotation_speed*frame_time_f;
+			gfx_model_rotate(ship, -ship_rotation_speed*frame_time_f, rot_axis);
+			gfx_camera_rotate(-ship_rotation_speed*frame_time_f, rot_axis);
+		}
+
+		if(keyboard_state[SDL_SCANCODE_D])
+		{
+			ship_current_rotation.y=ship_current_rotation.y+ship_rotation_speed*frame_time_f;
+			gfx_model_rotate(ship, ship_rotation_speed*frame_time_f, rot_axis);
+			gfx_camera_rotate(ship_rotation_speed*frame_time_f, rot_axis);
+		}
+
+		SDL_PollEvent(&event);
+		switch(event.type)
+		{
+			case SDL_QUIT:
+				quit=1;
+				break;
+		}
+	}
+
+	/*ship and camera*/
+	gfx_update_model_location(ship, ship_pos);
+	cam_location=ship_pos;
+	cam_location.x=cam_location.x+40*sin(-ship_current_rotation.y);
+	cam_location.y=cam_location.y+10;
+	cam_location.z=cam_location.z-40*cos(-ship_current_rotation.y);
+	gfx_camera_location(cam_location);
+
+	/*rotating stuff*/
 	rot_axis.x=0;
 	rot_axis.y=1;
 	rot_axis.z=0;
-	r=0.001;
+	r=cube_rotation_speed*frame_time_f;
 	gfx_model_rotate(cube_origin, r, rot_axis);
-
 	for(int i=0; i<1000; i++)
 	{
 		gfx_model_rotate(cube[i], r, rot_axis);
