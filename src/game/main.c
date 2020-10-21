af58b4b7422a3df3bf9f846ab12775440ad4640f
tevoran <j.pfeiffer93@gmx.de>
2020-10-21 22:11:35 +0200

    mainfile code cleanup complete

--- src/game/main.c
+++ src/game/main.c
@@ -53,45 +53,6 @@ int main(int argc, char **argv[])
 	player.model=gfx_load_model("data/models/stealth/stealth.obj", player.position);
 	gfx_model_load_texture("data/textures/stealth.bmp", player.model);
 
-	/*ship*/
-	struct vec3f rot_axis;
-	rot_axis.x=0;
-	rot_axis.y=1;
-	rot_axis.z=0;
-	struct vec3f ship_pos;
-	float r;
-	struct vec3f ship_current_rotation;
-
-	/*placement if host*/
-		ship_pos.x=0;
-		ship_pos.y=0;
-		ship_pos.z=40;
-		ship_current_rotation.x=0;
-		ship_current_rotation.y=0;
-		ship_current_rotation.z=0;
-
-
-	struct model *ship=gfx_load_model("data/models/stealth/stealth.obj",ship_pos);
-	r=2*PI;
-	gfx_model_rotate(ship, r, rot_axis);
-	gfx_model_load_texture("data/textures/stealth.bmp", ship);
-
-
-	/*other player*/
-	struct vec3f other_pos={0,0,0};
-	struct model *other_ship=gfx_load_model("data/models/stealth/stealth.obj",other_pos);
-	r=2*PI;
-	gfx_model_rotate(other_ship, r, rot_axis);
-	gfx_model_load_texture("data/textures/stealth.bmp", other_ship);
-
-	/*time related stuff*/
-	clock_t old_time=clock();
-	clock_t new_time, frame_time;
-	float frame_time_f;
-
-	float ship_speed=50;
-	float ship_rotation_speed=2.3;
-
 
 	int quit=0;
 	while(!quit) /*while not closing the window the main loop is continuing*/
@@ -99,12 +60,6 @@ int main(int argc, char **argv[])
 	engine_fps_count(ENGINE_TRUE,0);
 
 	game_player_controls_input(&player);
-
-	/*determining time*/
-	new_time=clock();
-	frame_time=new_time-old_time;
-	old_time=new_time;
-	frame_time_f=(float)frame_time/CLOCKS_PER_SEC;
 
 
 	/*handling input via SDL2*/
